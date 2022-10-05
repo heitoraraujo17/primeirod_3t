529fd47bb49b81eac3bf5b74fe5af633a3271f4f
vigzx <108942147+heitoraraujo17@users.noreply.github.com>
2022-10-05 11:08:45 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,1 +1,26 @@
+let tabuada = 2;
+
+function escreve(){
+      //2 x 1 = 2
+      document.write(tabuada + " x 1 = "+(tabuada*1)+"<br>");
+      document.write(tabuada + " x 2 = "+(tabuada*1)+"<br>");
+      document.write(tabuada + " x 3 = "+(tabuada*1)+"<br>");
+      document.write(tabuada + " x 4 = "+(tabuada*1)+"<br>");
+      document.write(tabuada + " x 5 = "+(tabuada*1)+"<br>");
+      document.write(tabuada + " x 6 = "+(tabuada*1)+"<br>");
+      document.write(tabuada + " x 7 = "+(tabuada*1)+"<br>");
+      document.write(tabuada + " x 8 = "+(tabuada*1)+"<br>");
+      document.write(tabuada + " x 9 = "+(tabuada*1)+"<br>");
+      document.write(tabuada + " x 10 = "+(tabuada*1)+"<br>");
+}
+function repete(){
+    for(let i = 1; i <= 10; i++)
+       document.write("Tabuada do " + i + "<br>");
+       for(let j =1; j <=10; j++){
+           document.write(i + " X " + j + " = "+(j*i))
+       }
+
+}   
+        
+
 
